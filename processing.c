dacc7905520ddf7d09c18236d001b5c321a86f97
Nat H <74020864+MxSelfDestruct@users.noreply.github.com>
2020-11-13 11:00:04 -0800

    add header to processing.c

--- processing.c
+++ processing.c
@@ -1,3 +1,28 @@
+/*
+ * Pokedex
+ * 
+ * processing.c
+ * 
+ * Copyright 2020 Nat Hofelt <nat@VEGA>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
 //Makes things slightly more readable
 #define SEARCH	0
 #define WRITE	1
